8e732b301a0a66d4bb8cedb321b74ca97ff77dfe
vinay-debug <84920398+vinay-debug@users.noreply.github.com>
2022-02-26 19:24:30 +0530

    Create areaOfCircle.js

---
+++ areaOfCircle.js
@@ -0,0 +1,18 @@
+function areaOfCircle(r) {
+    pi = 3.14;
+    return (pi * r * r)
+}
+console.log(areaOfCircle(1))
+
+const areaOfCircle1 = (r) => {
+    pi = 3.14;
+    return (pi * r * r);
+}
+console.log(areaOfCircle1(2))
+
+const area = function areaOfCircle2(r) {
+    pi = 3.14;
+    return (pi * r * r);
+}
+const a = area(4)
+console.log(a)
